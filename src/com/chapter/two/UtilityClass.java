49fbaa3527b25e932cef29b28f966e743ccadf13
Evan <ht.peach0302@gmail.com>
2015-11-25 10:00:47 +0800

    UtilityClass noninstantliability

---
+++ src/com/chapter/two/UtilityClass.java
@@ -0,0 +1,16 @@
+package com.chapter.two;
+
+/**
+ * 
+ * @Description: 不能实例化的类
+ *
+ * @author Evan
+ *
+ * @date Nov 25, 2015 9:58:32 AM
+ */
+public class UtilityClass {
+	//Suppress default constructor for noninstantiability
+	private UtilityClass () {
+		throw new AssertionError();
+	}
+}
